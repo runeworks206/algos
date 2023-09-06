75da496bf26798e8ef599fa52f010323005aee51
runeworks206 <runeworks206@gmail.com>
2023-09-07 02:24:49 +1000

    Modified viewer html

--- scripts/rw-algos.js
+++ scripts/rw-algos.js
@@ -1,26 +1,39 @@
 runeworks = typeof runeworks != 'undefined' ? runeworks : {}
 
 runeworks.algos = (function() {
+  let meta = {
+    binarySearch: {
+      name  : 'Binary search',
+      inputs: {
+        "array" : 'json',
+        "target": 'string',
+      },
+    },
+  }
 
   // Binary search
-  let binarySearch = function(space, left, right, target) {
+  let binarySearch = function(array, target, left, right) {
+    // Define left/right
+    if (typeof left  == 'undefined') { left  = 0 }
+    if (typeof right == 'undefined') { right = array.length - 1}
+
     // Exhausted search space
     if (left > right) { return -1 }
 
     let mid = Math.floor((left + right) / 2)
 
     // Binary
-    if (target == space[mid]) {
+    if (target == array[mid]) {
       return mid
-    } else if (target < space[mid]) {
-      return binarySearch(space, left, mid - 1, target)
+    } else if (target < array[mid]) {
+      return binarySearch(array, target, left, mid - 1)
     } else {
-      return binarySearch(space, mid + 1, right, target)
+      return binarySearch(array, target, mid + 1, right)
     }
   }
 
-
   return {
+    meta: meta,
     binarySearch: binarySearch,
   }
 })()
